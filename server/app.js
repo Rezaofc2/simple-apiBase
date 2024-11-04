ebf586b9240ed7b62a54aea095c00556f4e5a324
krizz <154887919+krsna081@users.noreply.github.com>
2024-11-04 16:19:24 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,6 @@ const kotakHytam = require("../pages/fitures/blackbox.js");
 const tiktod = require("../pages/fitures/tiktok.js");
 const mediapire = require("../pages/fitures/mediafire.js");
 const igedl = require("../pages/fitures/instagram.js");
-const ephoto = require("../pages/fitures/ephoto.js"); // Pastikan Anda sudah menempatkan file ephoto.js di path yang benar
 const { limit, checkBanned } = require("../declaration/rateLimit.jsx");
 
 const app = express();
@@ -36,104 +35,6 @@ app.get("/downloader/instagramDL", limit, (req, res) => {
 });
 app.get("/downloader/mediafireDL", limit, (req, res) => {
   mediapire(req, res);
-});
-
-// Menambahkan rute untuk ephoto
-app.get("/ephoto/glitchtext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/writetext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/advancedglow", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/typographytext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/pixelglitch", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/neonglitch", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/flagtext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/flag3dtext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/deletingtext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/blackpinkstyle", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/glowingtext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/underwatertext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/logomaker", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/cartoonstyle", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/papercutstyle", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/watercolortext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/effectclouds", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/blackpinklogo", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/gradienttext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/summerbeach", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/luxurygold", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/multicoloredneon", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/sandsummer", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/galaxywallpaper", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/1917style", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/makingneon", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/royaltext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/amongustext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/freecreate", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/rainytext", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/galaxystyle", limit, (req, res) => {
-  ephoto(req, res);
-});
-app.get("/ephoto/lighteffects", limit, (req, res) => {
-  ephoto(req, res);
 });
 
 module.exports = app;
