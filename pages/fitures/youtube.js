98b34c21107f115795ac1c07467cf9081d6e852b
krizz <154887919+krsna081@users.noreply.github.com>
2024-11-04 17:57:12 +0700

    Create youtube.js

---
+++ pages/fitures/youtube.js
@@ -0,0 +1,33 @@
+const axios = require("axios")
+const allowedApiKeys = require("../../declaration/arrayKey.jsx")
+
+module.exports = async (req, res) => {
+  let urls = req.query.urls
+  if (!urls) {
+    return res.status(400).json({
+      error: "Url yt Nya Mana?"
+    })
+  }
+
+  if (!apiKey || !allowedApiKeys.includes(apiKey)) {
+    return res.status(403).json({
+      error: "Input Parameter Apikey !"
+    })
+  }
+
+  let url = `https://api.betabotz.eu.org/api/download/ytmp3?url=${urls}&apikey=${process.env.btz}`
+
+  try {
+    const response = await axios.get(url)
+    const videoUrl = response.data.result
+    res.status(200).json({
+      status: true,
+      creator: "krizz",
+      data: videoUrl
+    })
+  } catch (e) {
+    res.status(500).json({
+      error: "Ada masalah, coba lagi nanti"
+    })
+  }
+}
