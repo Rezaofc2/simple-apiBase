5d53326a4cca93aa504d5aef9abd0e38a5b74afe
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2024-11-06 19:34:37 +0700

    Update ambatron.js

--- pages/fitures/ambatron.js
+++ pages/fitures/ambatron.js
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
     const response = await ambatronAi(q)
     res.status(200).json({
       status: true,
-      creator: "krizz",
+      creator: "RezaOffc",
       data: response
     })
   } catch (error) {
